3826b5796b0576bb718f44d5e9a13eecab0177ba
woobin huh <madgb89@naver.com>
2018-07-18 18:01:52 +0900

    animate

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$(".wrapper").hide();
 
 	$(".cats-open").on('click', function() {
+		$( 'html, body' ).stop().animate( { scrollTop : '100' } );
 		$(".wrapper").slideToggle(400);
-		$(window).scrollTop(100);
 	});
 });
